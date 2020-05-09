e81cb22d2cd1f506addd37808ad160120c382bfc
sid-thakur47 <sidthakur258@gmail.com>
2020-05-09 12:38:20 +0530

    ADDED:Test to know top batting average of Batsmen playing IPL

---
+++ CricketLeagueAnalysis/src/test/java/com/bl/cricketanalyzer/CricketAnalyzerTest.java
@@ -0,0 +1,33 @@
+package com.bl.cricketanalyzer;
+
+import com.bl.cricketanalyzer.constants.FilePaths;
+import com.bl.cricketanalyzer.exception.CricketAnalyserException;
+import com.bl.cricketanalyzer.model.BatsMenCSV;
+import com.bl.cricketanalyzer.service.CricketAnalyzer;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CricketAnalyzerTest implements FilePaths {
+    @Test
+    public void givenIPLData_CSVFile_ReturnsCorrectRecords() {
+        try {
+            CricketAnalyzer cricketAnalyzer = new CricketAnalyzer();
+            int numOfRecords = cricketAnalyzer.loadCricketData( BATSMEN_DATA );
+            Assert.assertEquals( 100, numOfRecords );
+        } catch (CricketAnalyserException e) {
+        }
+    }
+    @Test
+    public void givenWhenCricketData_ShouldReturn_TopBattingAverage() {
+        try {
+            CricketAnalyzer cricketAnalyzer = new CricketAnalyzer();
+            cricketAnalyzer.loadCricketData( BATSMEN_DATA );
+            String sortedCensusData = cricketAnalyzer.getFieldWiseData( "average" );
+            BatsMenCSV[] censusCSV = new Gson().fromJson( sortedCensusData, BatsMenCSV[].class );
+            Assert.assertEquals( "MS Dhoni", censusCSV[0].player );
+        } catch (CricketAnalyserException | JsonSyntaxException e) {
+        }
+    }
+}
